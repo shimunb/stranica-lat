1830ea0cf5389d77543231976f0b9318531728c7
shimunb <62101064+shimunb@users.noreply.github.com>
2021-11-09 21:09:08 +0100

    Add files via upload

---
+++ dikte.js
@@ -0,0 +1,16 @@
+function dikte(hrv, lat) {
+    var htmld = ''
+    htmld = `
+    <div class="dikta">
+    <div class="dikta-kartica">
+      <div class="dikta-hrv">
+        <p>hrv</p>
+      </div>
+      <div class="dikta-lat">
+        <p>lat</p>
+      </div>
+    </div>
+  </div>`
+    return htmld
+
+  }
